55f5338a9c793c19dcc4b54226a44b1b3827c19a
Luca Dandois <luca.dandois@student.kdg.be>
2024-04-11 16:21:17 +0200

    dynamic

--- src/js/loadTemplates.ts
+++ src/js/loadTemplates.ts
@@ -1,5 +1,7 @@
 import footer from '../assets/dynamicHtml/footer.html';
+import navbar from '../assets/dynamicHtml/navbar.html';
 
-export function loadFooter() : void {
+export function loadFooter(): void {
     document.getElementsByTagName('footer')[0].innerHTML = footer;
+    document.getElementById('navbar')!.innerHTML = navbar;
 }
